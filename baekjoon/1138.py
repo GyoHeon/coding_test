2c46b2b1e2cfe7a6c130d1fa1a902d747b655248
GyoHeon Lee <ghl12078@gmail.com>
2021-11-04 11:18:27 +0900

    new problem add

--- baekjoon/1138.py
+++ baekjoon/1138.py
@@ -1,2 +1,26 @@
-P∏n = int(input()
-li = list(map(int,input(),split( )))
+n = int(input())
+array = [i+1 for i in range(n)]
+li = list(map(int,input().split()))
+
+dic = dict()
+
+for i in range(n):
+    dic[i+1] = li[i]
+
+for i in range(n):
+    array1 = array[:i]
+    num = dic[i+1]
+    for _ in range(10):
+        for j in array1:
+            if dic[j] <= num:
+                try:
+                    array1.remove(j)
+                    num += 1
+                except:
+                    pass
+        dic[i+1] = num
+
+# value 로 정렬
+dic = dict(sorted(dic.items(), key= lambda item: item[1]))
+
+print(*list(dic.keys()))
