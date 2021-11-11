0490bbcc733d4f0bbbaf446eff7f148996b89e2d
GyoHeon Lee <ghl12078@gmail.com>
2021-11-12 02:44:20 +0900

    add new problem

---
+++ programmers/다트 게임.js
@@ -0,0 +1,4 @@
+let dartResult = "1S2D*3T";
+for (let i of dartResult) {
+  console.log(i);
+}
---
+++ programmers/키패드 누르기.js
@@ -0,0 +1,30 @@
+let numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2];
+let hand = "left";
+let l = 0;
+let r = 0;
+hand === "left" ? (r = 0.5) : (l = 0.5);
+let nowLeft = 10;
+let nowRight = 12;
+let answer = "";
+for (let i of numbers) {
+  i === 0 ? (i = 11) : null;
+  if (i === 1 || i === 4 || i === 7) {
+    answer += "L";
+    nowLeft = i;
+  } else if (i === 3 || i === 6 || i === 9) {
+    answer += "R";
+    nowRight = i;
+  } else {
+    if (
+      Math.round(Math.abs(nowLeft - i) / 3) + (Math.abs(nowLeft - i) % 3) + l <=
+      Math.round(Math.abs(nowRight - i) / 3) + (Math.abs(nowRight - i) % 3) + r
+    ) {
+      answer += "L";
+      nowLeft = i;
+    } else {
+      answer += "R";
+      nowRight = i;
+    }
+  }
+}
+console.log(answer);
