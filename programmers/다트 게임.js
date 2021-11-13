6fde3bf7fc706eaec69792cbe64a95dbeb501765
GyoHeon Lee <ghl12078@gmail.com>
2021-11-14 01:12:13 +0900

    add new problem

--- programmers/다트 게임.js
+++ programmers/다트 게임.js
@@ -1,4 +1,11 @@
 let dartResult = "1S2D*3T";
+let score = 0;
+let num = 0;
 for (let i of dartResult) {
-  console.log(i);
+  if (parseInt(i) !== NaN) {
+    console.log(parseInt(i));
+  } else {
+    score += parseInt(i);
+  }
 }
+console.log(score);
--- programmers/행렬의 덧셈.js
+++ programmers/행렬의 덧셈.js
@@ -1,10 +1,30 @@
-function sum(a, b) {
-  let len = a[0].length;
+function solution(arr1, arr2, sign) {
+  let len = arr1[0].length;
   let c = [];
-  for (let i in a) {
+  for (let i in arr1) {
     c[i] = new Array(len);
     for (let j = 0; j < len; j++) {
-      c[i][j] = a[i][j] + b[i][j];
+      c[i][j] = sign[i][j]
+        ? arr1[i][j] + arr2[i][j]
+        : -(arr1[i][j] + arr2[i][j]);
     }
   }
+  return c;
 }
+console.log(
+  solution(
+    [
+      [1, 2],
+      [2, 3],
+    ],
+    [
+      [3, 4],
+      [5, 6],
+    ],
+    [
+      [true, true],
+      [true, false],
+    ]
+  )
+);
+console.log(solution([[1], [2]], [[3], [4]], [[false], [false]]));
