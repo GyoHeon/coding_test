287e20ffef28241fbf3ca0ba588e743565de50d8
GyoHeon Lee <ghl12078@gmail.com>
2021-11-22 00:16:43 +0900

    add reduce method

--- programmers/음양 더하기.js
+++ programmers/음양 더하기.js
@@ -1,7 +1,15 @@
-function solution(absolutes, signs) {
+function solution1(absolutes, signs) {
   let answer = 0;
   for (let i in absolutes) {
     answer += absolutes[i] * (signs[i] ? 1 : -1);
   }
+  return answer;
+}
+
+function solution2(absolutes, signs) {
+  let answer = absolutes.reduce(
+    (acc, _, i) => (acc += absolutes[i] * (signs[i] ? 1 : -1)),
+    0
+  );
   return answer;
 }
