48ea15928adfbeb03acb6c12788d6c7dd819a612
GyoHeon Lee <ghl12078@gmail.com>
2021-12-22 05:43:56 +0900

    add new problem

--- programmers/level 2/다리를 지나는 트럭.js
+++ programmers/level 2/다리를 지나는 트럭.js
@@ -1,9 +1,26 @@
 let bridge_length = 2;
 let weight = 10;
 let truck_weights = [7, 4, 5, 6];
-let n = truck_weights.length;
 
-for (let i = 0; i < n; i++) {
-  let truck = truck_weights.shift();
-  console.log(truck);
+function solution(bridge_length, weight, truck_weights) {
+  var answer = 0;
+  let sum = 0;
+  let bridge = Array.from(Array(bridge_length).fill(0));
+  while (truck_weights.length) {
+    if (sum + truck_weights[0] - bridge[0] <= weight) {
+      sum -= bridge.shift();
+      const newTruck = truck_weights.shift();
+      bridge.push(newTruck);
+      sum += newTruck;
+      answer++;
+    } else {
+      sum -= bridge.shift();
+      bridge.push(0);
+      answer++;
+    }
+    console.log(bridge);
+  }
+  return answer + bridge_length;
 }
+
+console.log(solution(bridge_length, weight, truck_weights));
