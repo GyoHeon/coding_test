e8ad4b72d9ff3f0807a33278d1fdb630eb516eca
GyoHeonLee <ghl12078@gmail.com>
2021-12-27 05:05:04 +0900

    add new problem

--- programmers/level 2/짝지어 제거하기.js
+++ programmers/level 2/짝지어 제거하기.js
@@ -1,12 +1,15 @@
 function solution(s) {
   s = s.split("");
   if (s.length % 2) return 0;
-  const regExp = /(.)\1/;
-  for (let i = 0; i < s.length - 1; i) {
-    if (s[i] === s[i + 1]) {
+  let stack = [s.shift()];
+  for (let i of s) {
+    if (stack[stack.length - 1] === s[i]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
     }
   }
-  return s ? 0 : 1;
+  return stack.length ? 0 : 1;
 }
 
 console.log(solution("baabaa"));
