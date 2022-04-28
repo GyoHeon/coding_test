7253ab80870c38efe881614e784fd6bf1585661c
GyoHeonLee <ghl12078@gmail.com>
2022-04-28 23:37:03 +0900

    new problem solving way

--- leetcode/easy/575.js
+++ leetcode/easy/575.js
@@ -6,3 +6,9 @@ var distributeCandies = function (candyType) {
   });
   return Math.min(answer, types.length);
 };
+
+var distributeCandies = function (candyType) {
+  let answer = Math.floor(candyType.length / 2);
+  const types = new Set(candyType);
+  return Math.min(answer, types.size);
+};
