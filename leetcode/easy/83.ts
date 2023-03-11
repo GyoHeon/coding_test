56e1636752aeb74841dfbafbd0166771cd3a9f40
GyoHeonLee <ghl12078@gmail.com>
2023-03-12 01:13:32 +0900

    Add new problem

---
+++ leetcode/easy/83.ts
@@ -0,0 +1,21 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head) return null;
+  let node = head;
+  while (node.next) {
+    if (node.val === node.next.val) {
+      node.next = node.next.next;
+    } else {
+      node = node.next;
+    }
+  }
+  return head;
+}
